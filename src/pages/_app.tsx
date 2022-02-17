d4a90096edb7bed4d999610d863c921d468379b6
Ema Lorenzo <aemalorenzo@gmail.com>
2022-02-17 15:58:51 -0300

    inject mediaqueries into styled

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyles } from '@/components/GlobalStyles';
+import { QUERIES } from '@/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Component {...pageProps} />;
+    <ThemeProvider theme={{ queries: QUERIES }}>
+      <Component {...pageProps} />
       <GlobalStyles />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/index.ts
@@ -0,0 +1,7 @@
+const BREAKPOINTS = {
+  tabletMin: 550,
+};
+
+export const QUERIES = {
+  mobile: `(max-width: ${BREAKPOINTS.tabletMin}px)`,
+};
