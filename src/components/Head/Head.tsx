cdb9d9de8a7624d82962ea2efc960eb5fe6c35cc
Ema Lorenzo <aemalorenzo@gmail.com>
2022-02-17 21:28:02 -0300

    allow indexing

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -9,6 +9,7 @@ export const Head = () => (
       content="basement, swag, supplies, tshirt, cap, hoddie"
     />
     <meta name="author" content="Emanuel Lorenzo" />
+    <meta name="robots" content="follow, index" />
     <link rel="icon" href="/favicon.ico" />
   </NextHead>
 );
