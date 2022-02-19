918bbe5f9b1a4096860d26e7b5f9c8710994f4d2
Ema Lorenzo <aemalorenzo@gmail.com>
2022-02-18 21:49:51 -0300

    document serving pre bundled styled components

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,81 +1,7 @@
-// import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-// import { ServerStyleSheet } from 'styled-components';
-
-// export default class Document extends NextDocument {
-//   // static async getInitialProps(ctx) {
-//   //   const sheet = new ServerStyleSheet();
-//   //   const originalRenderPage = ctx.renderPage;
-
-//   //   try {
-//   //     ctx.renderPage = () =>
-//   //       originalRenderPage({
-//   //         enhanceApp: (App) => (props) =>
-//   //           sheet.collectStyles(<App {...props} />),
-//   //       });
-
-//   //     const initialProps = await Document.getInitialProps(ctx);
-//   //     return {
-//   //       ...initialProps,
-//   //       styles: (
-//   //         <>
-//   //           {initialProps.styles}
-//   //           {sheet.getStyleElement()}
-//   //         </>
-//   //       ),
-//   //     };
-//   //   } finally {
-//   //     sheet.seal();
-//   //   }
-//   // }
-
-//   render() {
-//     return (
-//       <Html lang="en">
-//         <Head>
-//           <link
-//             rel="preload"
-//             href="/fonts/basement-grotesqueBlack.woff2"
-//             as="font"
-//             type="font/woff2"
-//             crossOrigin="anonymous"
-//           />
-//           <link href="/images/favicons/favicon.ico" rel="icon" />
-//           <link href="/images/favicons/site.webmanifest" rel="manifest" />
-//           <link
-//             href="/images/favicons/apple-touch-icon.png"
-//             rel="apple-touch-icon"
-//             sizes="180x180"
-//           />
-//           <link
-//             href="/images/favicons/favicon-32x32.png"
-//             rel="icon"
-//             sizes="32x32"
-//             type="image/png"
-//           />
-//           <link
-//             href="/images/favicons/favicon-16x16.png"
-//             rel="icon"
-//             sizes="16x16"
-//             type="image/png"
-//           />
-//           <link color="#000000" rel="mask-icon" />
-//           <meta content="#000000" name="theme-color" />
-//           <meta content="#000000" name="msapplication-TileColor" />
-//         </Head>
-
-//         <body>
-//           <Main />
-//           <NextScript />
-//         </body>
-//       </Html>
-//     );
-//   }
-// }
-
-import Document from 'next/document';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-export default class MyDocument extends Document {
+export default class Document extends NextDocument {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -87,7 +13,7 @@ export default class MyDocument extends Document {
             sheet.collectStyles(<App {...props} />),
         });
 
-      const initialProps = await Document.getInitialProps(ctx);
+      const initialProps = await NextDocument.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -100,5 +26,48 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/basement-grotesqueBlack.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link href="/images/favicons/favicon.ico" rel="icon" />
+          <link href="/images/favicons/site.webmanifest" rel="manifest" />
+          <link
+            href="/images/favicons/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
+          />
+          <link
+            href="/images/favicons/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="/images/favicons/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
+          <link color="#000000" rel="mask-icon" />
+          <meta content="#000000" name="theme-color" />
+          <meta content="#000000" name="msapplication-TileColor" />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
