5898a819dd960958ea1873f4f48dee3b61fdce9e
Ema Lorenzo <aemalorenzo@gmail.com>
2022-03-02 15:09:01 -0300

    removed postprocess

--- src/components/Cubic/Cubic.tsx
+++ src/components/Cubic/Cubic.tsx
@@ -7,8 +7,6 @@ import { extend, useFrame } from '@react-three/fiber';
 import { useViewportScroll } from 'framer-motion';
 /* @ts-ignore */
 import { MeshLine, MeshLineMaterial } from 'three.meshline';
-import { Outline, Glitch, EffectComposer } from '@react-three/postprocessing';
-import { BlendFunction, Resizer, KernelSize } from 'postprocessing';
 
 extend({ MeshLine, MeshLineMaterial });
 
@@ -39,12 +37,11 @@ export const Cubic = ({ ...props }) => {
   });
 
   return (
-    <>
-      <group ref={group} {...props} dispose={null} scale={0.15}>
-        {/* @ts-ignore  */}
-        <mesh geometry={nodes.Cube.geometry} material={materials.Material}>
-          {/* geometry={nodes.Cube.geometry} material={materials.Material} */}
-          {/* <meshLine attach="geometry" points={nodes.Cube.geometry} />
+    <group ref={group} {...props} dispose={null} scale={0.15}>
+      {/* @ts-ignore  */}
+      <mesh geometry={nodes.Cube.geometry} material={materials.Material}>
+        {/* geometry={nodes.Cube.geometry} material={materials.Material} */}
+        {/* <meshLine attach="geometry" points={nodes.Cube.geometry} />
         <meshLineMaterial
           attach="material"
           // transparent
@@ -54,29 +51,11 @@ export const Cubic = ({ ...props }) => {
           dashArray={0.05}
           dashRatio={0.95}
         /> */}
-        </mesh>
-        <lineSegments geometry={edges} renderOrder={100}>
-          <lineBasicMaterial color="white" linewidth={20} />
-        </lineSegments>
-      </group>
-      <EffectComposer>
-        <Outline
-          selection={[group]} // selection of objects that will be outlined
-          selectionLayer={10} // selection layer
-          blendFunction={BlendFunction.SCREEN} // set this to BlendFunction.ALPHA for dark outlines
-          // patternTexture={null} // a pattern texture
-          edgeStrength={5} // the edge strength
-          pulseSpeed={0.0} // a pulse speed. A value of zero disables the pulse effect
-          visibleEdgeColor={0xffffff} // the color of visible edges
-          hiddenEdgeColor={0x22090a} // the color of hidden edges
-          width={Resizer.AUTO_SIZE} // render width
-          height={Resizer.AUTO_SIZE} // render height
-          kernelSize={KernelSize.LARGE} // blur kernel size
-          blur={false} // whether the outline should be blurred
-          xRay // indicates whether X-Ray outlines are enabled
-        />
-      </EffectComposer>
-    </>
+      </mesh>
+      <lineSegments geometry={edges} renderOrder={100}>
+        <lineBasicMaterial color="white" linewidth={20} />
+      </lineSegments>
+    </group>
   );
 };
 
--- src/components/Scene/Scene.tsx
+++ src/components/Scene/Scene.tsx
@@ -4,26 +4,23 @@ import { Glitch, EffectComposer } from '@react-three/postprocessing';
 
 import { Cubic } from '@/components';
 
-export const Scene = () => {
-  return (
-    <Canvas dpr={[1, 2]} camera={{ zoom: 50, position: [0, 0, 100] }}>
-      <Suspense fallback={null}>
-        {/* <OrthographicCamera makeDefault /> */}
-        <Cubic position={[-1.8, -0.5, 1]} rotation={[0.7, 1, 0]} />
-        <Cubic position={[2, -0.1, 1]} rotation={[1, 2, 0]} />
+export const Scene = () => (
+  <Canvas dpr={[1, 2]} camera={{ zoom: 50, position: [0, 0, 100] }}>
+    <Suspense fallback={null}>
+      {/* <OrthographicCamera makeDefault /> */}
+      <Cubic position={[-1.8, -0.5, 1]} rotation={[0.7, 1, 0]} />
+      <Cubic position={[2, -0.1, 1]} rotation={[1, 2, 0]} />
 
-        {/* <EffectComposer> */}
-        {/* <Glitch
-            delay={[2, 5]} // min and max glitch delay
-            duration={[0.2, 0.5]} // min and max glitch duration
-            strength={[0.01, 0.03]} // min and max glitch strength
-            // mode={GlitchMode.SPORADIC} // glitch mode
-            active // turn on/off the effect (switches between "mode" prop and GlitchMode.DISABLED)
-            ratio={0.85} // Threshold for strong glitches, 0 - no weak glitches, 1 - no strong glitches.
-          /> */}
-
-        {/* </EffectComposer> */}
-      </Suspense>
-    </Canvas>
-  );
-};
+      {/* <EffectComposer>
+        <Glitch
+          delay={[2, 5]} // min and max glitch delay
+          duration={[0.2, 0.5]} // min and max glitch duration
+          strength={[0.05, 0.1]} // min and max glitch strength
+          // mode={GlitchMode.SPORADIC} // glitch mode
+          active // turn on/off the effect (switches between "mode" prop and GlitchMode.DISABLED)
+          ratio={0.85} // Threshold for strong glitches, 0 - no weak glitches, 1 - no strong glitches.
+        />
+      </EffectComposer> */}
+    </Suspense>
+  </Canvas>
+);
